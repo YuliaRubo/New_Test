5cb3dd8fafeb27757009934a6d7f547872ac5e68
Ulya Ivancova <ulya-rubo@mail.ru>
2022-06-06 12:54:05 +0200

    correct UserDaoImpl

--- src/main/java/com/example/test1/dao/UserDaoImpl.java
+++ src/main/java/com/example/test1/dao/UserDaoImpl.java
@@ -2,19 +2,31 @@ package com.example.test1.dao;
 
 import com.example.test1.dto.Chat;
 import com.example.test1.dto.Users;
+import com.example.test1.rowMapper.UserRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.EmptySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
 public class UserDaoImpl implements UserDao {
 
 
    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-  private  String ADD_USER= "INSERT INTO USERS(userName, createdAt) VALUES(:userName, :createdAt)";
+  private  String ADD_USER= "INSERT INTO USERS(userName) VALUES(:userName)";
 
     @Override
     public Users addNewUser(Users user) {
-        namedParameterJdbcTemplate.update(ADD_USER, new BeanPropertySqlParameterSource(user));
+        Map<String,Object> params = new HashMap<>();
+        params.put("userName", user.getUserName());
+        //params.put("createdAt", user.getCreatedAt());
+
+        namedParameterJdbcTemplate.update(ADD_USER, params);
         return user;
     }
 
