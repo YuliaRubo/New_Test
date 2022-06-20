97752237baec3f710cc17128aaaa8d4ef1f986dd
Ulya Ivancova <ulya-rubo@mail.ru>
2022-06-21 01:03:24 +0200

    User

--- src/main/java/com/example/test1/dto/Users.java
+++ src/main/java/com/example/test1/dto/Users.java
@@ -1,8 +1,11 @@
 package com.example.test1.dto;
 
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import sun.plugin2.message.Message;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Users {
 
@@ -10,20 +13,34 @@ public class Users {
     private String userName;
     private Timestamp createdAt;
 
+    private List<Chat> chatList;
 
+    private List<Message> massageList;
 
 
     public Users() {
     }
-
 
     public Users(int userId, String userName, Timestamp createdAt) {
         this.userId = userId;
         this.userName = userName;
         this.createdAt = createdAt;
     }
+  public void addChatToUsers(Chat chat){
+        if(chatList==null){
+            chatList = new ArrayList<>();
+        }
+        chatList.add(chat);
 
+  }
 
+  public void addMessageToUser(Message message){
+        if(massageList==null){
+            massageList = new ArrayList<>();
+        }
+        massageList.add(message);
+
+  }
 
     public int getUserId() {
         return userId;
