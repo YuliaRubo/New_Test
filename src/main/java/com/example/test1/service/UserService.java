20538cf1767ce8554581fc60f227b60d58423f2c
Ulya Ivancova <ulya-rubo@mail.ru>
2022-06-20 12:49:10 +0200

    UserService

--- src/main/java/com/example/test1/service/UserService.java
+++ src/main/java/com/example/test1/service/UserService.java
@@ -1,8 +1,15 @@
 package com.example.test1.service;
 
 import com.example.test1.dto.Users;
+import org.apache.catalina.User;
+
+import java.util.List;
 
 public interface UserService {
 
-    public Users addNewUser(Users user);
+    public int addNewUser(Users user);
+    List<Users> getAllUsersFromUsers();
+    Users getUserById(int userId);
+
+    //public Users getUserById(int id);
 }
