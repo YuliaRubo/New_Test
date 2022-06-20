262a67aab4ddaed5f82e7565a6f5883ac17e71c0
Ulya Ivancova <ulya-rubo@mail.ru>
2022-06-20 12:47:34 +0200

    ChatUser

---
+++ src/main/java/com/example/test1/dto/ChatUser.java
@@ -0,0 +1,31 @@
+package com.example.test1.dto;
+
+public class ChatUser {
+
+    private Users users;
+    private Chat chat;
+
+    public ChatUser(Users users, Chat chat) {
+        this.users = users;
+        this.chat = chat;
+    }
+
+    public ChatUser() {
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+
+    public Chat getChat() {
+        return chat;
+    }
+
+    public void setChat(Chat chat) {
+        this.chat = chat;
+    }
+}
