7577bac128ccf8b693153e80da1b85db48c7004f
Ulya Ivancova <ulya-rubo@mail.ru>
2022-06-20 12:49:27 +0200

    UserServiceImpl

--- src/main/java/com/example/test1/service/UserServiceImpl.java
+++ src/main/java/com/example/test1/service/UserServiceImpl.java
@@ -5,17 +5,35 @@ import com.example.test1.dto.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private UserDao userDao;
+
     @Autowired
     public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
     }
 
+
     @Override
-    public Users addNewUser(Users user) {
-      return userDao.addNewUser(user);
+    public int addNewUser(Users user) {
+        return userDao.save(user);
     }
+
+    @Override
+    public List<Users> getAllUsersFromUsers() {
+        return userDao.getAllUsersFromUsers();
+    }
+
+    @Override
+    public Users getUserById(int userId) {
+        return null;
+    }
+
+    //@Override
+    //public Users getUserById(int userId) {
+        //return userDao.getUserById(userId);    }
 }
