bc02ea88f50a410650523f04f1a5e8ad134f426f
Ulya Ivancova <ulya-rubo@mail.ru>
2022-06-20 12:45:02 +0200

    UserController

--- src/main/java/com/example/test1/controller/UserController.java
+++ src/main/java/com/example/test1/controller/UserController.java
@@ -4,11 +4,18 @@ import com.example.test1.dto.Users;
 import com.example.test1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import sun.rmi.runtime.Log;
+
+import java.util.logging.Logger;
 
 @Controller
 public class UserController {
+
+   // private  final  static Logger LOGGER= Logger.getLogger(String.valueOf(Controller.class));
 
     private UserService userService;
     @Autowired
@@ -17,7 +24,14 @@ public class UserController {
     }
 
     @PostMapping("/add")
-    public Users addNewUser(@RequestBody Users users){
-       return userService.addNewUser(users);
+    public int  addNewUser(@RequestBody Users user){
+        //LOGGER.info("save good");
+        return userService.addNewUser(user);
     }
+
+    @GetMapping("/users")
+    public void getUllUsers(){
+        userService.getAllUsersFromUsers();
+    }
+
 }
